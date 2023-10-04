02e992d6f9aa2a1ebd6071b9a9f05eb60474b618
Shahoriar Nazim Rifat <snrifat@incieto.com>
2023-10-03 20:29:10 -0700

    Change header title

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@
 const siteMetadata = {
   title: 'Test Blog',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'Demo Site- Nazim',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
